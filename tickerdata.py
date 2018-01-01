f237101ff257610de9499ddc468c38f52572a1b3
LAPTOP-JHL\Erik <equintanilla@hawk.iit.edu>
2018-01-01 13:30:41 -0600

    Mainframe setup complete, switched to python 3.5 for compatibility with PyQt

--- tickerdata.py
+++ tickerdata.py
@@ -1,6 +1,7 @@
 import urllib
 import json
 import requests
+import codecs
 
 class Crypto(object):
 	def __init__(self,name,symbol, price_usd,rank,percent_change_1h, percent_change_24h):
@@ -14,16 +15,13 @@ class Crypto(object):
 		rstring = self.rank + '. ' + self.name + ' (' + self.symbol + ') ' + self.price_usd + ' ' + self.percent_change_1h + ' ' + self.percent_change_24h + '\n'
 		return rstring
 
-
-page = urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/")
-pre_json = page.read()
+page  =requests.get("https://api.coinmarketcap.com/v1/ticker/")
 #fetch json file, read, and then load.
-loaded_json_list = json.loads(pre_json)
 #note that only the top 10 cryptocurrencies are going to be shown.
+loaded_json_list = page.json()
 currency =[]
 for x in range(10):
 	current = loaded_json_list[x]
 	crypto_obj = Crypto(current["name"], current["symbol"], current["price_usd"], current["rank"], current["percent_change_1h"], current["percent_change_24h"])
 	currency.append(crypto_obj)
-
 #great, now we have objects in a list that we can call!
--- tickergui.py
+++ tickergui.py
@@ -5,17 +5,18 @@
 # Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
+from tickerdata import currency
+from PyQt5 import QtCore, QtGui, QtWidgets\
 
-from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 440)
+        MainWindow.resize(790, 450)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
-        self.tableWidget.setGeometry(QtCore.QRect(0, -9, 801, 391))
+        self.tableWidget.setGeometry(QtCore.QRect(0, -9, 801, 410))
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(6)
         self.tableWidget.setRowCount(10)
@@ -51,6 +52,19 @@ class Ui_MainWindow(object):
         self.tableWidget.setHorizontalHeaderItem(4, item)
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(5, item)
+        #helper function to create all columns
+        def create_col(col_num):
+            for x in range(10):
+                item = QtWidgets.QTableWidgetItem() #need this 
+                self.tableWidget.setItem(x,col_num,item)
+
+        #create all columns to be filled in!
+        def create_all_cols():
+            for x in range(6):
+                create_col(x)
+
+        create_all_cols()
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 860, 26))
@@ -98,11 +112,33 @@ class Ui_MainWindow(object):
         item = self.tableWidget.horizontalHeaderItem(2)
         item.setText(_translate("MainWindow", "Symbol"))
         item = self.tableWidget.horizontalHeaderItem(3)
-        item.setText(_translate("MainWindow", "New Column"))
+        item.setText(_translate("MainWindow", "Price (USD)"))
         item = self.tableWidget.horizontalHeaderItem(4)
         item.setText(_translate("MainWindow", "% Change (1H)"))
         item = self.tableWidget.horizontalHeaderItem(5)
         item.setText(_translate("MainWindow", "% Change (24H)"))
+
+        #namecolumn
+        for x in range(10):
+            item = self.tableWidget.item(x, 1)
+            item.setText(_translate("MainWindow", currency[x].name))
+        for x in range(10):
+            item = self.tableWidget.item(x, 0)
+            item.setText(_translate("MainWindow", currency[x].rank))
+        for x in range(10):
+            item = self.tableWidget.item(x, 2)
+            item.setText(_translate("MainWindow", currency[x].symbol))
+        for x in range(10):
+            item = self.tableWidget.item(x, 3)
+            item.setText(_translate("MainWindow", "$"+currency[x].price_usd))
+        for x in range(10):
+            item = self.tableWidget.item(x, 4)
+            item.setText(_translate("MainWindow", currency[x].percent_change_1h))
+        for x in range(10):
+            item = self.tableWidget.item(x, 5)
+            item.setText(_translate("MainWindow", currency[x].percent_change_24h))
+
+
         self.menuTicker.setTitle(_translate("MainWindow", "Ticker"))
 if __name__ == "__main__":
     import sys
