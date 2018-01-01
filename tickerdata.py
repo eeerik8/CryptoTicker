e9afa7a8752d3b641c0d3f098fdd3eba0f287755
LAPTOP-JHL\Erik <equintanilla@hawk.iit.edu>
2018-01-01 00:55:01 -0600

    Creaing gui

--- tickerdata.py
+++ tickerdata.py
@@ -2,26 +2,7 @@ import urllib
 import json
 import requests
 
-page = urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/")
-pre_json = page.read()
-#fetch json file, read, and then load.
-loaded_json_list = json.loads(pre_json)
-#note that only the top 10 cryptocurrencies are going to be shown.
-for x in range(10):
-	current = loaded_json_list[x]
-	print(current["name"])
-	print(current["symbol"])
-	print(current["price_usd"])
-	print(current["rank"])
-	print(current["percent_change_1h"])
-	print(current["percent_change_24h"])
-
-class crypto:
-	symbol=""
-	price_usd = 0.00
-	rank = 0
-	percent_change_24h = 0.00
-	percent_change_1h =0.00
+class Crypto(object):
 	def __init__(self,name,symbol, price_usd,rank,percent_change_1h, percent_change_24h):
 		self.name = name
 		self.symbol = symbol
@@ -29,5 +10,20 @@ class crypto:
 		self.rank = rank
 		self.percent_change_24h = percent_change_24h
 		self.percent_change_1h = percent_change_1h
+	def __repr__(self):
+		rstring = self.rank + '. ' + self.name + ' (' + self.symbol + ') ' + self.price_usd + ' ' + self.percent_change_1h + ' ' + self.percent_change_24h + '\n'
+		return rstring
 
-def make_crypto
+
+page = urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/")
+pre_json = page.read()
+#fetch json file, read, and then load.
+loaded_json_list = json.loads(pre_json)
+#note that only the top 10 cryptocurrencies are going to be shown.
+currency =[]
+for x in range(10):
+	current = loaded_json_list[x]
+	crypto_obj = Crypto(current["name"], current["symbol"], current["price_usd"], current["rank"], current["percent_change_1h"], current["percent_change_24h"])
+	currency.append(crypto_obj)
+
+#great, now we have objects in a list that we can call!
